d7b2434527ae23a025f1cb33a490c6cb6b567680
Tatiana <yanta89@mail.ru>
2016-11-05 23:58:07 +0500

    Task6 updated

--- Task6Reflection/Task1/Application/Program.cs
+++ Task6Reflection/Task1/Application/Program.cs
@@ -15,12 +15,12 @@ namespace Application
         
         static void Main(string[] args)
         {
-            foreach (var element in Directory.GetFiles(new FileInfo(@"..\..\..\Solution\").FullName, "*.dll"))
+            foreach (var element in Directory.GetFiles( new FileInfo(@"..\..\..\Solution\").FullName, "*.dll") )
             {
                 var dllPlugin = Assembly.LoadFile(element);
-                foreach (var type in dllPlugin.GetTypes())
+                foreach ( var type in dllPlugin.GetTypes() )
                 {
-                    if (type.GetInterfaces().Contains(typeof(IPlugin)))
+                    if ( type.GetInterfaces().Contains(typeof(IPlugin) ) )
                     {
                         var pluginInstance = (IPlugin)Activator.CreateInstance(type);
                         Console.WriteLine(pluginInstance.Name);
@@ -28,8 +28,9 @@ namespace Application
                 }
             }
 
-            Expression<Func<double, double>> expressionExample = x => Math.Sin( 5 * (x + 8) );
+            Expression< Func<double, double> > expressionExample = x => Math.Sin( 5 * (x + 8) );
             Console.WriteLine( Differentiator.Differentiate(expressionExample) );
+            
         }
     }
 }
